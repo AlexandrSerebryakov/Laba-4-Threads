c87b6b90733dac0eab1d0ba8476499adaef7daa8
AlexandrSerebryakov <common190516@mail.ru>
2016-11-12 22:10:25 +0600

    Create hierarchy.

---
+++ src/main/java/com/epam/alex/main/Runner.java
@@ -0,0 +1,10 @@
+package com.epam.alex.main;
+
+/**
+ * Created by AlexandrSerebryakov on 12.11.2016.
+ */
+public class Runner {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/epam/alex/main/entity/Ship.java
@@ -0,0 +1,7 @@
+package com.epam.alex.main.entity;
+
+/**
+ * Created by AlexandrSerebryakov on 12.11.2016.
+ */
+public class Ship {
+}
