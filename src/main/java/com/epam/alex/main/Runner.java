f7cadb7ace47d401f7b758d358af46cf9c5a6b61
AlexandrSerebryakov <common190516@mail.ru>
2016-11-14 13:49:52 +0600

    Add variables.

--- src/main/java/com/epam/alex/main/Runner.java
+++ src/main/java/com/epam/alex/main/Runner.java
@@ -4,7 +4,7 @@ package com.epam.alex.main;
  * Created by AlexandrSerebryakov on 12.11.2016.
  */
 public class Runner {
-    public static void main(String[] args) {
+    public  static void main(String[] args) {
 
     }
 }
--- src/main/java/com/epam/alex/main/entity/Ship.java
+++ src/main/java/com/epam/alex/main/entity/Ship.java
@@ -4,4 +4,6 @@ package com.epam.alex.main.entity;
  * Created by AlexandrSerebryakov on 12.11.2016.
  */
 public class Ship {
+    private int shipCapacity;
+    private int shipNumberContainers;
 }
