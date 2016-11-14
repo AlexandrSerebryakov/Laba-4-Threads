de62bbec1a2aee174f8f3a773d13b07204cf5803
AlexandrSerebryakov <common190516@mail.ru>
2016-11-14 14:11:36 +0600

    Edit main class.

--- src/main/java/com/epam/alex/main/Runner.java
+++ src/main/java/com/epam/alex/main/Runner.java
@@ -1,10 +1,22 @@
 package com.epam.alex.main;
 
+import com.epam.alex.main.entity.FirstBerth;
+import com.epam.alex.main.entity.SecondBerth;
+import com.epam.alex.main.entity.ThirdBerth;
+
 /**
  * Created by AlexandrSerebryakov on 12.11.2016.
  */
 public class Runner {
     public  static void main(String[] args) {
+        FirstBerth firstBerth = new FirstBerth();
+        SecondBerth secondBerth = new SecondBerth();
+        ThirdBerth thirdBerth = new ThirdBerth();
+
+        new Thread(firstBerth).start();
+        new Thread(secondBerth).start();
+        new Thread(thirdBerth).start();
+
 
     }
 }
